834c08c32651acf9a8d5f39136fe4731aaed53cd
swift14370864 <yzy86075222@126.com>
2018-10-16 15:50:01 +0800

    更改了picture界面

--- src/components/diagnosisPicture.vue
+++ src/components/diagnosisPicture.vue
@@ -1,68 +1,58 @@
 <template>
   <div class="picture">
-    <el-carousel indicator-position="outside">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3>{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="病例图片" name="first">
+        <div>
+          <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="病例处方" name="second">
+        <el-row>
+          <div class="prescription">
+            <h2 class="">处方如下：</h2>
+            <div class="">
+              <div class="text item">
+                1. xxxxxxxxxx
+              </div>
+              <div class="text item">
+                2. xxxxxxxxxxxxxxxxxxx
+              </div>
+              <div class="text item">
+                3. xxxxxxxxxxx
+              </div>
+              <div class="text item">
+                4. xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              </div>
+              <div class="text item">
+                5. xxxxxxxxxxx
+              </div>
+            </div>
+          </div>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
+    <!--<el-carousel indicator-position="outside">-->
+      <!--<el-carousel-item v-for="item in 4" :key="item">-->
+        <!--<h3>{{ item }}</h3>-->
+      <!--</el-carousel-item>-->
+    <!--</el-carousel>-->
   </div>
 </template>
 
 <script>
   export default {
-    name: 'HelloWorld',
-    data () {
+    data() {
       return {
-        msg: '病例列表与诊断历史图片界面展示',
-        tableData3: [{
-          date: '2018-05-07',
-          name: '王小虎',
-          hospital: '嘉定中心医院',
-          diagnose_res: '湿疹',
-          prescription: '盐酸左氧氟沙星0.4g静脉输液',
-        },{
-          date: '2018-07-05',
-          name: '王小虎',
-          hospital: '嘉定中心医院',
-          diagnose_res: '湿疹',
-          prescription: '上清片',
-        },{
-          date: '2018-07-12',
-          name: '王小虎',
-          hospital: '嘉定中心医院',
-          diagnose_res: '湿疹',
-          prescription: '甲硝锉片',
-        },{
-          date: '2018-07-27',
-          name: '王小虎',
-          hospital: '嘉定中心医院',
-          diagnose_res: '湿疹',
-          prescription: '盐酸左氧氟沙星0.4g静脉输液',
-        },{
-          date: '2018-08-23',
-          name: '王小虎',
-          hospital: '同济大学校医院',
-          diagnose_res: '粉刺',
-          prescription: '维生素C0.5g静脉输液',
-        },{
-          date: '2018-08-30',
-          name: '王小虎',
-          hospital: '嘉定中心医院',
-          diagnose_res: '粉刺',
-          prescription: '盐酸左氧氟沙星0.4g静脉输液',
-        },{
-          date: '2018-09-07',
-          name: '王小虎',
-          hospital: '同济大学校医院嘉定分院',
-          diagnose_res: '股癣',
-          prescription: '股癣药膏',
-        },{
-          date: '2018-09-22',
-          name: '王小虎',
-          hospital: '嘉定中心医院',
-          diagnose_res: '股癣',
-          prescription: '股癣药膏',
-        }]
+        activeName: 'first'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
       }
     }
   }
@@ -99,5 +89,8 @@
   }
   .picture {
     width: 50%;
+  }
+  .prescription {
+    text-align: left;
   }
 </style>
