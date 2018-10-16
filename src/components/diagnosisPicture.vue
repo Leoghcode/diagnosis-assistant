f9ef2e1c09bdec99e7d9080ee00744f71e8b4964
swift14370864 <yzy86075222@126.com>
2018-10-16 16:22:38 +0800

    完善了picture界面，使用了imageviewer封装。

--- src/components/diagnosisPicture.vue
+++ src/components/diagnosisPicture.vue
@@ -3,11 +3,7 @@
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="病例图片" name="first">
         <div>
-          <el-carousel indicator-position="outside">
-            <el-carousel-item v-for="item in 4" :key="item">
-              <h3>{{ item }}</h3>
-            </el-carousel-item>
-          </el-carousel>
+          <image-viewer :img-list="images"></image-viewer>
         </div>
       </el-tab-pane>
       <el-tab-pane label="病例处方" name="second">
@@ -44,11 +40,26 @@
 </template>
 
 <script>
+  import ImageViewer from "./common/ImageViewer";
+  import skin_1 from '../assets/skin/skin_1.jpg'
+  import skin_2 from '../assets/skin/skin_2.jpg'
+  import skin_3 from '../assets/skin/skin_3.jpg'
+  import skin_4 from '../assets/skin/skin_4.jpg'
+  const images = [
+    {image: skin_1, name: 'skin_1.jpg', result: '皮肤病A', level: '轻微', advice: '自行买皮肤药'},
+    {image: skin_2, name: 'skin_2.jpg', result: '皮肤病B', level: '中度', advice: '就医，买药'},
+    {image: skin_3, name: 'skin_3.jpg', result: '皮肤病C', level: '严重', advice: '立即就医'},
+    {image: skin_4, name: 'skin_4.jpg', result: '皮肤病D', level: '紧急', advice: '立即就医'}
+  ];
   export default {
     data() {
       return {
-        activeName: 'first'
+        activeName: 'first',
+        images: images
       };
+    },
+    components: {
+        ImageViewer: ImageViewer
     },
     methods: {
       handleClick(tab, event) {
