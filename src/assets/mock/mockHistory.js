c594d818b57818ec718ae20faf8abd414a211fe5
PaulWeeeel <weiyaopaul@163.com>
2018-10-18 14:38:42 +0800

    remend to track additional files

---
+++ src/assets/mock/mockHistory.js
@@ -0,0 +1,49 @@
+export default [{
+  date: '2018-05-07',
+  name: '王小虎',
+  hospital: '嘉定中心医院',
+  diagnose_res: '湿疹',
+  prescription: '盐酸左氧氟沙星0.4g静脉输液',
+},{
+  date: '2018-07-05',
+  name: '王小虎',
+  hospital: '嘉定中心医院',
+  diagnose_res: '湿疹',
+  prescription: '上清片',
+},{
+  date: '2018-07-12',
+  name: '王小虎',
+  hospital: '嘉定中心医院',
+  diagnose_res: '湿疹',
+  prescription: '甲硝锉片',
+},{
+  date: '2018-07-27',
+  name: '王小虎',
+  hospital: '嘉定中心医院',
+  diagnose_res: '湿疹',
+  prescription: '盐酸左氧氟沙星0.4g静脉输液',
+},{
+  date: '2018-08-23',
+  name: '王小虎',
+  hospital: '同济大学校医院',
+  diagnose_res: '粉刺',
+  prescription: '维生素C0.5g静脉输液',
+},{
+  date: '2018-08-30',
+  name: '王小虎',
+  hospital: '嘉定中心医院',
+  diagnose_res: '粉刺',
+  prescription: '盐酸左氧氟沙星0.4g静脉输液',
+},{
+  date: '2018-09-07',
+  name: '王小虎',
+  hospital: '同济大学校医院嘉定分院',
+  diagnose_res: '股癣',
+  prescription: '股癣药膏',
+},{
+  date: '2018-09-22',
+  name: '王小虎',
+  hospital: '嘉定中心医院',
+  diagnose_res: '股癣',
+  prescription: '股癣药膏',
+}];
---
+++ src/assets/mock/mockPresTemplates.js
@@ -0,0 +1,66 @@
+const presOptions ={
+  '病A': ['处方A-1', '处方A-2', '处方A-3'],
+  '病B': ['处方B-1', '处方B-2'],
+  '病C': ['处方C-1', '处方C-2', '处方C-3'],
+  '病D': ['处方D-1'],
+  '病E': ['处方E-1', '处方E-2']
+};
+const presDetails = {
+  '处方A-1': [
+    {medicine: '药A', number: '12g'},
+    {medicine: '药B', number: '6片'},
+    {medicine: '药C', number: '2瓶'},
+    {medicine: '药D', number: '1盒'}
+  ],
+  '处方A-2': [
+    {medicine: '药D', number: '2.5g'},
+    {medicine: '药B', number: '3片'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+  '处方A-3': [
+    {medicine: '药A', number: '15g'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+  '处方B-1': [
+    {medicine: '药A', number: '12g'},
+    {medicine: '药B', number: '6片'},
+    {medicine: '药C', number: '2瓶'},
+    {medicine: '药D', number: '1盒'}
+  ],
+  '处方B-2': [
+    {medicine: '药D', number: '2.5g'},
+    {medicine: '药B', number: '3片'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+  '处方C-1': [
+    {medicine: '药A', number: '12g'},
+    {medicine: '药B', number: '6片'},
+    {medicine: '药C', number: '2瓶'},
+    {medicine: '药D', number: '1盒'}
+  ],
+  '处方C-2': [
+    {medicine: '药D', number: '2.5g'},
+    {medicine: '药B', number: '3片'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+  '处方C-3': [
+    {medicine: '药A', number: '15g'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+  '处方D-1': [
+    {medicine: '药A', number: '12g'},
+    {medicine: '药B', number: '6片'},
+    {medicine: '药C', number: '2瓶'},
+    {medicine: '药D', number: '1盒'}
+  ],
+  '处方E-1': [
+    {medicine: '药D', number: '2.5g'},
+    {medicine: '药B', number: '3片'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+  '处方E-2': [
+    {medicine: '药A', number: '15g'},
+    {medicine: '药C', number: '1瓶'}
+  ],
+};
+export {presOptions,presDetails};
