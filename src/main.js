2a278a12d9d6dac912fdf4f18427a1b1b7fdf9d0
Leo.qi <qbzhhxx1996@qq.com>
2018-10-27 15:15:49 +0800

    增加axios 和 api接口前缀。

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import echarts from 'echarts'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/index.css'
 
+Vue.prototype.$axios = axios
 Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 Vue.use(ElementUI)
